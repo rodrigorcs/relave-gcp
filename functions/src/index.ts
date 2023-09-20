1906e35714a20a33ced7b3e684f74d5e5b7bba20
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2023-09-19 23:14:00 -0300

    Hardcoded regions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,14 +4,13 @@ import { initializeApp } from "firebase-admin/app"
 import { stripeAction } from "./actions/stripe";
 import { defineSecret } from "firebase-functions/params";
 import { Secrets } from "./models/constants/secrets";
-import { Constants } from "./models/constants";
 
 const STRIPE_SK = defineSecret(Secrets.STRIPE_SK)
 const STRIPE_PK = defineSecret(Secrets.STRIPE_PK)
 
 initializeApp();
 
-export const createStripeCustomer = onRequest({ secrets: [STRIPE_SK, STRIPE_PK], region: Constants.GCP_DEFAULT_REGION }, async (req: Request, res: Response) => {
+export const createStripeCustomer = onRequest({ secrets: [STRIPE_SK, STRIPE_PK], region: 'southamerica-east1' }, async (req: Request, res: Response) => {
   const { customerStripeId, amount } = req.body
 
   try {
@@ -23,7 +22,7 @@ export const createStripeCustomer = onRequest({ secrets: [STRIPE_SK, STRIPE_PK],
   }
 });
 
-export const createStripePaymentIntent = onRequest({ secrets: [STRIPE_SK, STRIPE_PK], region: Constants.GCP_DEFAULT_REGION }, async (req: Request, res: Response) => {
+export const createStripePaymentIntent = onRequest({ secrets: [STRIPE_SK, STRIPE_PK], region: 'southamerica-east1' }, async (req: Request, res: Response) => {
   const { customerStripeId, amount } = req.body
 
   try {
--- functions/src/models/constants/index.ts
+++ functions/src/models/constants/index.ts
@@ -1,4 +1,3 @@
 export const Constants = {
   STRIPE_API_VERSION: '2023-08-16',
-  GCP_DEFAULT_REGION: 'southamerica-east1'
 } as const
