c2198f254c8e9f5f0fe637e594b46259de91ef47
Rodrigo Costa <rodrigo.costa.ssa@gmail.com>
2023-10-14 09:48:32 -0300

    Added default region to functions

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -4,6 +4,9 @@ import { initializeApp } from "firebase-admin/app"
 import { defineSecret } from "firebase-functions/params";
 import { Secrets } from "./models/constants/secrets";
 import { createStripeCustomerHandler, addOrderToScheduleHandler, createStripePaymentIntentHandler, stripeWebhookHandler, getOrdersAvailabilityByDateHandler } from "./handlers";
+import { setGlobalOptions } from "firebase-functions/v2";
+
+setGlobalOptions({ region: "southamerica-east1" });
 
 const STRIPE_SK = defineSecret(Secrets.STRIPE_SK)
 const STRIPE_PK = defineSecret(Secrets.STRIPE_PK)
@@ -11,10 +14,10 @@ const STRIPE_WEBHOOK_SECRET = defineSecret(Secrets.STRIPE_WEBHOOK_SECRET)
 
 initializeApp();
 
-export const createStripeCustomer = onRequest({ secrets: [STRIPE_SK, STRIPE_PK], region: 'southamerica-east1' }, createStripeCustomerHandler)
-export const createStripePaymentIntent = onRequest({ secrets: [STRIPE_SK, STRIPE_PK], region: 'southamerica-east1' }, createStripePaymentIntentHandler);
-export const stripeWebhook = onRequest({ secrets: [STRIPE_SK, STRIPE_WEBHOOK_SECRET], region: 'southamerica-east1' }, stripeWebhookHandler);
-export const getOrdersAvailabilityByDate = onRequest({ region: 'southamerica-east1' }, getOrdersAvailabilityByDateHandler);
+export const createStripeCustomer = onRequest({ secrets: [STRIPE_SK, STRIPE_PK] }, createStripeCustomerHandler)
+export const createStripePaymentIntent = onRequest({ secrets: [STRIPE_SK, STRIPE_PK] }, createStripePaymentIntentHandler);
+export const stripeWebhook = onRequest({ secrets: [STRIPE_SK, STRIPE_WEBHOOK_SECRET] }, stripeWebhookHandler);
+export const getOrdersAvailabilityByDate = onRequest(getOrdersAvailabilityByDateHandler);
 
 export const addOrderToSchedule = onDocumentUpdated('orders/{orderId}', addOrderToScheduleHandler)
 
